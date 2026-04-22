52de418109cde9c260f2e1e7e9f5d29bcea6f15b
Bekbol <bekbol.bv@gmail.com>
2026-04-22 16:48:22 +0500

    about-info fix text style

--- src/entities/About/ui/AboutInfo/AboutInfo.tsx
+++ src/entities/About/ui/AboutInfo/AboutInfo.tsx
@@ -1,9 +1,9 @@
 import { Section } from "@/shared/ui/Section";
 import { IAboutDataImages } from "../../model/types";
-import { Container } from "@/shared/ui/Container";
 import { env } from "@/shared/config/env";
 import { Heading } from "@/shared/ui/Heading";
 import { Description } from "@/shared/ui/Description";
+import { Title } from "@/shared/ui/Title";
 
 interface IProps {
   title: string;
@@ -18,15 +18,15 @@ function AboutInfo(props: IProps) {
     <Section
       className="my-40 md:my-60 min-h-[70vh] md:min-h-screen relative p-0!"
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.07), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9) 70%), url('${env.baseUrl}${images[0].image_path}')`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.07), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8) 95%), url('${env.baseUrl}${images[0].image_path}')`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="absolute bottom-30 left-[50%] translate-x-[-50%] text-center text-white max-w-[1200] w-full p-20">
-        <Heading className="mb-30">{title}</Heading>
-        <Description>{description}</Description>
+      <div className="absolute bottom-15 left-[50%] translate-x-[-50%] text-center text-white max-w-[1200] w-full p-20">
+        <Heading className="mb-15 md:mb-30">{title}</Heading>
+        <Title variyant="medium">{description}</Title>
       </div>
     </Section>
   );
