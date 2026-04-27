b856f1424546653bd51bb1db0b6758248adc51ea
Bekbol <bekbol.bv@gmail.com>
2026-04-27 11:44:50 +0500

    fix PhoneBtn

--- src/shared/ui/PhoneBtn/ui/PhoneBtn.tsx
+++ src/shared/ui/PhoneBtn/ui/PhoneBtn.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { clsx } from "@/shared/lib/clsx";
 import { FaPhoneAlt } from "react-icons/fa";
 import { ReactNode } from "react";
+import { useContextContact } from "@/app/provider/ContactProvider";
 
 interface IProps {
   className?: string;
@@ -9,10 +12,11 @@ interface IProps {
 
 function PhoneBtn(props: IProps) {
   const { children, className = "" } = props;
+  const { phone } = useContextContact();
 
   return (
     <a
-      href={`tel:${children ?? "+998933672555"}`}
+      href={`tel:${children ?? phone}`}
       className={clsx(
         [
           "bg-secondary-300  transition text-white rounded-6 inline-flex items-center gap-2 text-description-small py-2 px-3 md:text-description-medium hover:bg-secondary-400",
@@ -24,7 +28,7 @@ function PhoneBtn(props: IProps) {
         },
       )}
     >
-      <FaPhoneAlt size={17} /> {children ?? `+998 93 367 25 55`}
+      <FaPhoneAlt size={15} /> {children ?? phone}
     </a>
   );
 }
