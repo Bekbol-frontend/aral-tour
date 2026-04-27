b3ab0422cff351f55c3eae91cd6dc15ca22c37c3
Bekbol <bekbol.bv@gmail.com>
2026-04-27 12:25:40 +0500

    fix footer social

--- src/widgets/Footer/ui/FooterSocial/FooterSocial.tsx
+++ src/widgets/Footer/ui/FooterSocial/FooterSocial.tsx
@@ -4,20 +4,20 @@ import { useContextContact } from "@/app/provider/ContactProvider";
 import { FaTelegram, FaInstagram, FaYoutube, FaFacebook } from "react-icons/fa";
 
 function FooterSocial() {
-  const { telegram_url, instagram_url, youtube_url, facebook_url } =
+  const { telegram_username, instagram_username, youtube_url, facebook_name } =
     useContextContact();
 
   return (
     <div className="flex gap-5">
       <a
-        href={`https://t.me/${telegram_url}`}
+        href={`https://t.me/${telegram_username}`}
         target="_blank"
         className="btn p-0 text-title-medium w-[42] h-[42] bg-transparent text-white shadow-none border border-gray-250! inline-flex items-center justify-center hover:bg-secondary-400"
       >
         <FaTelegram />
       </a>
       <a
-        href={`https://www.instagram.com/${instagram_url}`}
+        href={`https://www.instagram.com/${instagram_username}`}
         target="_blank"
         className="btn p-0 text-title-medium w-[42] h-[42] bg-transparent text-white shadow-none border border-gray-250! inline-flex items-center justify-center hover:bg-secondary-400"
       >
@@ -31,7 +31,7 @@ function FooterSocial() {
         <FaYoutube />
       </a>
       <a
-        href={`https://www.facebook.com/${facebook_url}`}
+        href={`https://www.facebook.com/${facebook_name}`}
         target="_blank"
         className="btn p-0 text-title-medium w-[42] h-[42] bg-transparent text-white shadow-none border border-gray-250! inline-flex items-center justify-center hover:bg-secondary-400"
       >
